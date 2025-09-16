a2d0ca41a6dbf446acf71bf65eb8fd8251df3fc4
Adrián García <adri.garfan05@gmail.com>
2025-09-17 00:10:38 +0200

    Lo del servidor

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI ()
 
@@ -7,5 +8,14 @@ def raiz():
     return {"Mi backend esta listo"}
 
 @app.get("/buscar")
-def buscar(p: str):
+def buscar(q: str):
     return {"libros":[{"titulo":"El camino de los reyes, autor: Brandon Sanderson"}]}
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],  # puerto donde está tu frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
