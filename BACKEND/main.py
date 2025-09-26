40d00b8f3d194b8f559c66b6bf2e775ccc31dbc6
Adrián García <adri.garfan05@gmail.com>
2025-09-26 11:32:16 +0200

    buscador de las baldas

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -1,29 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from fastapi import HTTPException
 import books.book
 
 app = FastAPI ()
-
-@app.get("/")
-def raiz():
-    return {"Mi backend esta listo"}
 
 # Configuración de CORS
 origins = [
     "http://127.0.0.1:5500",  # frontend
     "http://localhost:5500"
 ]
-
-
-@app.get("/buscar")
-def buscar(texto: str):
-    resultados = books.book.search_books(texto)     
-    return {"resultados": resultados}
-
-
-
-
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://127.0.0.1:5500"],
@@ -31,3 +18,51 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+class Book(BaseModel):
+    titulo: str
+    autor: str
+    anio: str
+    portada: str
+    balda: int
+
+books_storage = []
+
+@app.get("/")
+def raiz():
+    return {"Mi backend esta listo"}
+
+@app.get("/buscar")
+def buscar(texto: str):
+    resultados = books.book.search_books(texto)     
+    return {"resultados": resultados}
+
+@app.post("/libros/{balda_id}")
+def agregar_libro_balda(balda_id: int, libro: Book):
+    if not (1 <= balda_id <= 10):
+        raise HTTPException(status_code=400, detail="Balda inválida")
+
+    # Preparamos el libro como lista
+    libro_data = [
+        libro.titulo,
+        libro.autor,
+        libro.anio,
+        libro.portada,
+        balda_id
+    ]
+
+    # Guardamos en un CSV distinto, ej. balda1.csv
+    nombre_csv = f"balda{balda_id}.csv"
+    books.book.add_books([libro_data], nombre_csv)  
+
+    return {"message": f"Libro añadido a balda {balda_id}", "libro": libro}
+
+
+# @app.get("/libros/balda/{balda_id}")
+# def obtener_libros_balda(balda_id: int):
+#     libros_balda = [libro for libro in books_storage if libro["balda"] == balda_id]
+#     return {"libros": libros_balda}
+
+
+
+
